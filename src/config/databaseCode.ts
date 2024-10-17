0d31ca2015c16ae34d402727fc20da415da4a9df
Mauro Felipe <mfelipxl@gmail.com>
2024-10-17 09:02:05 -0400

    create a new file databaseCode for export for another file, so fixing the problem about cannot use import, now i have two files to configure database, one for sequelize-cli, another for export for another files on code

--- src/config/databaseCode.ts
+++ src/config/databaseCode.ts
@@ -1,9 +1,11 @@
-require('dotenv/config');
+import { Options } from "sequelize"
+import dotenv from 'dotenv'
+dotenv.config()
 
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
 const port = parseInt(DB_PORT || "3306", 10)
 
-const config = {
+const config:Options = {
   dialect: 'mysql',
   host: DB_HOST,
   port,
@@ -16,4 +18,4 @@ const config = {
   },
 }
 
-module.exports = config
+export default config
--- src/database/sync.ts
+++ src/database/sync.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize'
-import config from '../config/database.ts'
+import config from '../config/databaseCode.ts'
+import { Options } from 'sequelize'
+const sequelizeOptions:Options = config
 
-const connection = new Sequelize(config)
+const connection = new Sequelize(sequelizeOptions)
 
 export default connection
