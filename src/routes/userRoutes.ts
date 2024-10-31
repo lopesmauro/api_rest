1823369d746fc5aebf1c6460395766bb2fb33422
Mauro Felipe <mfelipxl@gmail.com>
2024-10-31 10:31:25 -0400

    Removing de slug id, for security, so user cannot edit or make some change on another user.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,9 +5,9 @@ import { indexUser, showUser, storeUser, deleteUser, updateUser } from "../contr
 const route = Router()
 
 route.get("/", loginRequired, indexUser)
-route.get("/:id", loginRequired, showUser)
+route.get("/:", loginRequired, showUser)
 route.post("/", loginRequired, storeUser)
-route.delete("/:id", loginRequired, deleteUser)
-route.put("/:id", loginRequired, updateUser)
+route.delete("/", loginRequired, deleteUser)
+route.put("/", loginRequired, updateUser)
 
 export default route
